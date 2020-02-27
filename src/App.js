1237e3d76fdd4052140763055f7eef1d4032f508
lahawkin <lhawk1000@gmail.com>
2020-02-27 09:43:48 -0800

    got about me working with img but no bootstrap formatting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import photo from './images/balboa_park_me.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <h1>Laura Hawkins</h1>
+        <h2>About Me</h2>
+        <p> Hi I'm Laura Hawkins. I enjoy living in San Diego. I went to
+                                college at UCSD and I graduated with a degree in Computer Science. The UCSD Web Dev Bootcamp has helped reinforced my skills from school and  learn great new tools.
+                            </p>
+
+        <img src={photo} alt="" width="495" height="551"/> 
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
       </header>
     </div>
-  );
-}
-
-export default App;
+      );
+    }
+    
+    export default App;
