f7353a4c502d402ad13d777a2c4a912b3a73b43f
lahawkin <lhawk1000@gmail.com>
2020-02-27 10:16:53 -0800

    added in header, project, and footer components and removed bootstrap formatting todo: get contact and project pages working

--- src/App.js
+++ src/App.js
@@ -2,28 +2,42 @@ import React from 'react';
 import photo from './images/balboa_park_me.jpg';
 import './App.css';
 
+function Header(props) {
+  return <header className="App-header">
+    <h1>Laura Hawkins</h1>
+    <nav >
+      <a className="App=link" href="index.html" target="_blank" rel="noopener noreferrer">About Me</a> |
+      <a className="App=link" href="portfolio.html" target="_blank" rel="noopener noreferrer">Portfolio</a> |
+      <a className="App=link" href="contact.html" target="_blank" rel="noopener noreferrer">Contact</a>
+    </nav>
+  </header>
+}
+
+function Project(props) {
+  return <div className="App-body">
+    <h1>Laura Hawkins</h1>
+    <h2>About Me</h2>
+    <p> Hi I'm Laura Hawkins. I enjoy living in San Diego. I went to
+                            college at UCSD and I graduated with a degree in Computer Science. The UCSD Web Dev Bootcamp has helped reinforced my skills from school and  learn great new tools.
+                      </p>
+
+    <img src={photo} alt="" width="495" height="551" />
+
+  </div>;
+}
+
+function Footer(props) {
+  return <p>Copyright 2019</p>;
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Laura Hawkins</h1>
-        <h2>About Me</h2>
-        <p> Hi I'm Laura Hawkins. I enjoy living in San Diego. I went to
-                                college at UCSD and I graduated with a degree in Computer Science. The UCSD Web Dev Bootcamp has helped reinforced my skills from school and  learn great new tools.
-                            </p>
-
-        <img src={photo} alt="" width="495" height="551"/> 
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-      </header>
+      <Header />
+      <Project />
+      <Footer />
     </div>
-      );
-    }
-    
-    export default App;
+  );
+}
+
+export default App;
