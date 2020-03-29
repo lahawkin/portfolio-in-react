dbb2a8a1a2a9bf2093377d8f4d31fe23c8245669
lahawkin <lhawk1000@gmail.com>
2020-03-28 20:38:50 -0700

    changed colors, pictures still have problems though

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 ## My Portfolio
 This REACT portfolio highlights my projects from the coding bootcamp at UCSD.   
 
+## How to Run
+
+npm run start
+
 ## Credits
 
 Collaborated with the UCSD Extension Coding Bootcamp
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function Nav() {
   return (
+
+    
     <nav >
+    <img src="/images/sunflower_pic.png" alt="sunflower"></img>
     <a className="App=link" href="index.html" target="_blank" rel="noopener noreferrer">About Me</a> |
     <a className="App=link" href="portfolio.html" target="_blank" rel="noopener noreferrer">Portfolio</a> |
     <a className="App=link" href="contact.html" target="_blank" rel="noopener noreferrer">Contact</a>
